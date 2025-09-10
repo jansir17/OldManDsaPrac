c0dfae910548b4353721c823507b3eac30185003
jansir71 <jansir71@protonmail.com>
2025-09-10 08:46:08 +0600

    SWITCH

---
+++ src/BasicJava/Switch.java
@@ -0,0 +1,20 @@
+package BasicJava;
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fruit = sc.nextLine();
+
+        switch (fruit){
+            case "Mango":
+                System.out.println("You like Mango");
+                break;
+            case "Apple":
+                System.out.println("You like Apple");
+                break;
+            default:
+                System.out.println("you are not a fruit guy");
+        }
+    }
+}
