3ac31d924735ae3ac72ba2d1b9b10a0da84699c0
jansir71 <jansir71@protonmail.com>
2025-09-11 10:20:11 +0600

    Prime Number

---
+++ src/BasicJava/MethodFunctionString.java
@@ -0,0 +1,28 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class MethodFunctionString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int number = sc.nextInt();
+        boolean ans = isPrime(number);
+        System.out.println(ans);
+    }
+
+    static boolean isPrime(int prime) {
+        if (prime<=1){
+            return false;
+        }
+        int c = 2;
+        while(c*c<=prime)
+        {
+            if(prime%c == 0){
+                return false;
+            }
+            c++;
+        }
+        return c*c>prime;
+    }
+}
