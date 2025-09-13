2f1bbb29d4c87266455597ea6312ec4dfe3d0e5a
jansir71 <jansir71@protonmail.com>
2025-09-13 12:30:37 +0600

    Fixing Some code in Linear Search

--- src/Searching/LinearSearch.java
+++ src/Searching/LinearSearch.java
@@ -2,14 +2,37 @@ package Searching;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr01 = {3, 17, 23, 144, 5, 8, 15, 28};
+        //BasicLinear Search
+//        int[] arr01 = {3, 17, 23, 144, 5, 8, 15, 28};
+//        System.out.println("Element found in index number "+basicLinearSearch(arr01, -10));
+        //String
+        String name = "Mehrab";
+        char character = 'b';
+        isCharacterExists(name, character);
+
+    }
+
+    static int basicLinearSearch(int[] arr01, int element){
         int index = 0;
         for(int i: arr01) {
-            if (i == 28) {
+            if (i == element) {
                 System.out.println("Value found in index "+index);
-                break;
+                return index;
             }
             else index++;
         }
+        return -1;
     }
+
+    static void isCharacterExists(String s, char target){
+        if(s.length()== 0) System.out.println("No String found");
+        for(int i = 0; i<s.length(); i++) {
+            if (s.charAt(i) == target){
+                System.out.println("Found the Character in index "+i);
+                break;
+            }
+        }
+    }
+
+
 }
