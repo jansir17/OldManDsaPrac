2072e12fb8a637515b83da9e544aee9bd1c0dcc6
jansir71 <jansir71@protonmail.com>
2025-09-14 12:27:50 +0600

    orderAgnostic Approach of Binary Search

--- src/BinarySearch/AscDescBinary.java
+++ src/BinarySearch/AscDescBinary.java
@@ -2,31 +2,34 @@ package BinarySearch;
 
 public class AscDescBinary {
     public static void main(String[] args) {
-        int targetValue = 3;
+        int targetValue =55;
         int[] arr01 = {2, 5, 23, 45, 56, 76, 789, 898, 3455};
-        //int[] arr02 = {3453, 455, 43, 23,12, 5, 4, 3, 1};
-        System.out.println( findTargetValue(arr01, targetValue));
+        int[] arr02 = {3453, 455, 443, 323, 100, 55, 44, 33, 11, 1};
+        System.out.println(findTargetValue(arr02, targetValue));
     }
 
-    static int findTargetValue(int[] arr01, int targetValue) {
+    static int findTargetValue(int[] arr, int targetValue) {
         int start = 0;
-        int end = arr01.length-1;
-        System.out.println(end);
-        //boolean isAsc = arr01[0] < arr01.length - 1;
-        while(start<=end) {
-            int mid = start+(end-start)/2;
-            if (arr01[mid] == targetValue) return mid;
-            else {
-                if (targetValue < arr01[mid]) {
+        int end = arr.length - 1;
+        boolean isAsc = arr[0] < arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == targetValue) return mid;
+            if (isAsc) {
+                if (targetValue < arr[mid]) {
                     end = mid - 1;
-                } else if (targetValue > arr01[mid]){
+                } else {
                     start = mid + 1;
                 }
-                else return mid;
+            } else {
+                if (targetValue > arr[mid]) {
+                    end = mid - 1;
+                } else
+                    start = mid + 1;
             }
         }
         return -1;
-    }
 
+    }
 
 }
